23e132b4586433a44a5184040ba33e3bc0b7b62c
Coen ten Thije Boonkkamp <coen@coenttb.com>
2026-08-20 11:08:50 +0200

    Format and require Swift 6.4

--- Package.swift
+++ Package.swift
@@ -1,15 +1,15 @@
-// swift-tools-version: 6.3.3
+// swift-tools-version: 6.4
 
 import PackageDescription
 
 let package = Package(
     name: "swift-system-primitives",
     platforms: [
-        .macOS("27"),
-        .iOS("27"),
-        .tvOS("27"),
-        .watchOS("27"),
-        .visionOS("27")
+        .macOS(.v27),
+        .iOS(.v27),
+        .tvOS(.v27),
+        .watchOS(.v27),
+        .visionOS(.v27),
     ],
     products: [
         .library(
@@ -22,9 +22,18 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/swift-primitives/swift-cardinal-primitives.git", branch: "main"),
-        .package(url: "https://github.com/swift-primitives/swift-ordinal-primitives.git", branch: "main"),
-        .package(url: "https://github.com/swift-primitives/swift-memory-primitives.git", branch: "main"),
+        .package(
+            url: "https://github.com/swift-primitives/swift-cardinal-primitives.git",
+            branch: "main"
+        ),
+        .package(
+            url: "https://github.com/swift-primitives/swift-ordinal-primitives.git",
+            branch: "main"
+        ),
+        .package(
+            url: "https://github.com/swift-primitives/swift-memory-primitives.git",
+            branch: "main"
+        ),
     ],
     targets: [
         .target(
@@ -39,7 +48,10 @@ let package = Package(
             name: "System Primitives Test Support",
             dependencies: [
                 "System Primitives",
-                .product(name: "Cardinal Primitives Test Support", package: "swift-cardinal-primitives"),
+                .product(
+                    name: "Cardinal Primitives Test Support",
+                    package: "swift-cardinal-primitives"
+                ),
             ],
             path: "Tests/Support"
         ),
